c4dae35c33b8bc0e26a8fe4cfab41bc5c5ed9897
ZawMyoHtut <zawmyohtut222702@gmail.com>
2022-08-27 18:50:03 +0630

    testing api server completed

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
+
+const db = mongoose.connection
+
+db.on('error',error=>console.log(error))
+db.once('open',()=>console.log('connected to database'))
+
+app.use(express.json())
+
+const subscriberRouter = require('./routes/subscribers')
+app.use('/api',subscriberRouter)
+
+
+
+app.listen(3000,()=>{
+    console.log("host 3000")
+})
